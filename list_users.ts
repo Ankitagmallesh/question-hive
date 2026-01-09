85c8fec290867219027d57f41b75b225df0d03f4
Sanjay Raj <sanjayrajns@outlook.com>
2026-01-09 21:41:32 +0530

    chore: Add user listing script

---
+++ list_users.ts
@@ -0,0 +1,16 @@
+﻿
+import { db } from './apps/web/app/lib/db';
+import { users, questionPaperStatuses } from '@repo/db';
+
+async function main() {
+  const allUsers = await db.select().from(users);
+  console.log('--- USERS ---');
+  console.log(JSON.stringify(allUsers, null, 2));
+
+  const allStatuses = await db.select().from(questionPaperStatuses);
+  console.log('--- STATUSES ---');
+  console.log(JSON.stringify(allStatuses, null, 2));
+}
+
+main().catch(console.error);
+
